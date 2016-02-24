5c2ab4bbefaadd6ce2bbb7714b1e10a1813446ae
Rodrigo Núñez Mujica <rodrigo.nunez@innovation4security.com>
2016-02-24 12:47:46 +0100

    Added AwsRouteTableAssociationResource

--- src/tfstate/provider/aws/__init__.py
+++ src/tfstate/provider/aws/__init__.py
@@ -4,3 +4,4 @@ from tfstate.provider.aws.base import AwsResource
 from tfstate.provider.aws.aws_eip import AwsEipResource
 from tfstate.provider.aws.aws_internet_gateway import AwsInternetGatewayResource
 from tfstate.provider.aws.aws_route_table import AwsRouteTableResource
+from tfstate.provider.aws.aws_route_table_association import AwsRouteTableAssociationResource
--- unit_tests/test_tfstate/test_provider/test_aws/__init__.py
+++ unit_tests/test_tfstate/test_provider/test_aws/__init__.py
@@ -5,7 +5,7 @@ import unittest
 
 # Unit tests
 from unit_tests.test_tfstate.test_provider.test_aws import test_base, test_aws_eip, test_aws_internet_gateway
-from unit_tests.test_tfstate.test_provider.test_aws import test_aws_route_table
+from unit_tests.test_tfstate.test_provider.test_aws import test_aws_route_table, test_aws_route_table_association
 
 
 def suite():
@@ -14,6 +14,7 @@ def suite():
     suite.addTests(test_aws_eip.suite())
     suite.addTests(test_aws_internet_gateway.suite())
     suite.addTests(test_aws_route_table.suite())
+    suite.addTests(test_aws_route_table_association.suite())
     return suite
 
 if __name__ == '__main__':
