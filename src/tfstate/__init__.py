e26922fc2df0ca17829a8aad97103e7b4924e0ef
Rodrigo Núñez Mujica <rodrigo.nunez@innovation4security.com>
2016-12-02 13:09:31 +0100

    Bump version 0.4.0

--- src/tfstate/__init__.py
+++ src/tfstate/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 name = 'terraform_tfstate'
-version = '0.3.3'
+version = '0.4.0'
 description = 'Python library to parse a Terraform state file (tfstate)'
 author = 'Rodrigo Núñez Mujica'
 author_email = 'rnunezmujica@icloud.com'
